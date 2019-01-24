123a3eeff79ed97923f47e0041de83b9cd31d796
Tijana <tijana_vasic@yahoo.com>
2019-01-24 18:26:22 +0100

    Initial commit

---
+++ public_html/examples/assets/js/main.js
@@ -0,0 +1,41 @@
+
+
+//function changeDescription() {
+//   document.getElementById("description").innerHTML = "Molimo popunite formu sa vašim podacima."; 
+//}
+
+
+//function ageVerification() {
+//    //alert(ageInput);
+//    var ageInput = document.getElementById("age").value;
+//    var messageBoxElement = document.getElementById("message-box");
+//    
+//    if (ageInput >= 18) {
+//        messageBoxElement.innerHTML = "Korisnik je punoletan";
+//    } else {
+//        messageBoxElement.innerHTML = "Korisnik nije punoletan";
+//    }
+//}
+
+//function ageVerification() {
+//    //alert(ageInput);
+//    var ageInput = document.getElementById("age").value;
+//    var messageBoxElement = document.getElementById("message-box");
+//    
+//    if (ageInput >= 18) {
+//        messageBoxElement.innerHTML = "Korisnik je punoletan";
+//    } else {
+//        messageBoxElement.innerHTML = "Korisnik nije punoletan";
+//    }
+//}
+
+if(first>second){
+    document.comparation.result.value+"Prvi broj je veci";
+    }
+ else if(first<second) {
+    document.comparation.result.value="Prvi broj je manji";
+ else{
+      document.comparation.result.value="Brojevi su isti";  
+   }
+
+    
